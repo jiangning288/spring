5dc9c7170f96cac02f1fccb232a43d6b576bab3e
jiangning04 <jiangning04@meituan.com>
2021-08-14 22:34:54 +0800

    Initial commit

---
+++ my-spring/src/main/java/com/jiangning/Application.java
@@ -0,0 +1,11 @@
+package com.jiangning;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
+		UserDao userDao=ac.getBean(UserDao.class);
+		userDao.print();
+	}
+}
---
+++ my-spring/src/main/java/com/jiangning/UserDao.java
@@ -0,0 +1,10 @@
+package com.jiangning;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDao {
+	public void print(){
+		System.out.println("hello spring");
+	}
+}
